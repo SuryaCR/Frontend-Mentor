e5971289477655777f0436f105565c6f9dd5d688
Surya <suryamaverick1404@gmail.com>
2022-03-09 16:11:25 +0530

    new file added

---
+++ src/loop-studio.js
@@ -0,0 +1,8 @@
+document.getElementById('hamburger').addEventListener('click',()=>{
+    document.getElementById('subMenu').style.display='flex'
+    document.getElementById('subMenu').style.animationFillMode
+})
+
+document.getElementById('close').addEventListener('click',()=>{
+    document.getElementById('subMenu').style.display='none'
+})
